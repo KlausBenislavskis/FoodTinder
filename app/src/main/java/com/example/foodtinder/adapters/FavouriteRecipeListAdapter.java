8436eef65f93090a2fc43cce947e1987cb7a769e
JakubPlatzek <jakub.platzek@gmail.com>
2022-04-05 12:43:11 +0200

    Implementation of foldable list with some spicy animations

--- app/src/main/java/com/example/foodtinder/adapters/FavouriteRecipeListAdapter.java
+++ app/src/main/java/com/example/foodtinder/adapters/FavouriteRecipeListAdapter.java
@@ -1,82 +1,115 @@
 package com.example.foodtinder.adapters;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodtinder.R;
 import com.example.foodtinder.models.RecipeItemModel;
+import com.ramotion.foldingcell.FoldingCell;
 import com.squareup.picasso.Picasso;
 
+import java.util.HashSet;
 import java.util.List;
 
-public class FavouriteRecipeListAdapter extends RecyclerView.Adapter<FavouriteRecipeListAdapter.ViewHolder>{
+public class FavouriteRecipeListAdapter extends ArrayAdapter<RecipeItemModel> {
 
-    private List<RecipeItemModel> recipeItemModelList;
-    private OnClickListener listener;
+    private HashSet<Integer> unfoldedIndexes = new HashSet<>();
+    private View.OnClickListener listener;
 
-    public FavouriteRecipeListAdapter(List<RecipeItemModel> recipeItemModelList) {
-        this.recipeItemModelList = recipeItemModelList;
+    public FavouriteRecipeListAdapter(Context context, List<RecipeItemModel> recipeItemModelList) {
+        super(context, 0, recipeItemModelList);
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        View view = inflater.inflate(R.layout.fragment_recipe_item, parent, false);
-        return new ViewHolder(view);
-    }
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+        // get item for selected view
+        RecipeItemModel item = getItem(position);
+        // if cell is exists - reuse it, if not - create the new one from resource
+        FoldingCell cell = (FoldingCell) convertView;
+        ViewHolder viewHolder;
+        if (cell == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater vi = LayoutInflater.from(getContext());
+            cell = (FoldingCell) vi.inflate(R.layout.fragment_recipe_folding_item, parent, false);
+            // binding view parts to view holder
+            viewHolder.name = cell.findViewById(R.id.recipe_title);
+            viewHolder.name2 = cell.findViewById(R.id.recipe_title2);
+            viewHolder.image = cell.findViewById(R.id.recipe_small_image);
+            viewHolder.detailsButton = cell.findViewById(R.id.details_button);
+            cell.setTag(viewHolder);
+        } else {
+            // for existing cell set valid valid state(without animation)
+            if (unfoldedIndexes.contains(position)) {
+                cell.unfold(true);
+            } else {
+                cell.fold(true);
+            }
+            viewHolder = (ViewHolder) cell.getTag();
+        }
 
-    public void setOnClickListener(OnClickListener listener){
-        this.listener = listener;
-    }
+        if (null == item)
+            return cell;
 
-
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.name.setText(recipeItemModelList.get(position).getName());
+        // bind data from selected element to view through view holder
+        viewHolder.name.setText(item.getName());
+        viewHolder.name2.setText(item.getName());
         Picasso.get()
-                .load(recipeItemModelList.get(position).getImage())
+                .load(item.getImage())
                 .fit()
                 .centerCrop()
-                .into(holder.image);
+                .into(viewHolder.image);
 
-    }
+        // set custom btn handler for list item from that item
+        if (item.getRequestBtnClickListener() != null) {
+            viewHolder.detailsButton.setOnClickListener(item.getRequestBtnClickListener());
+        } else {
+            // (optionally) add "default" handler if no handler found in item
+            viewHolder.detailsButton.setOnClickListener(listener);
+        }
 
-    @Override
-    public int getItemCount() {
-        return recipeItemModelList.size();
+        return cell;
     }
 
     public interface OnClickListener{
         void OnClick(RecipeItemModel recipeItem);
     }
 
-    public List<RecipeItemModel> getItems() {
-        return recipeItemModelList;
+    public void registerFold(int position) {
+        unfoldedIndexes.remove(position);
     }
 
-    public void setItems(List<RecipeItemModel> items) {
-        this.recipeItemModelList = items;
+    public void registerUnfold(int position) {
+        unfoldedIndexes.add(position);
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder {
-        private final ImageView image;
-        private final TextView name;
+    public void registerToggle(int position) {
+        if (unfoldedIndexes.contains(position))
+            registerFold(position);
+        else
+            registerUnfold(position);
+    }
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            image = itemView.findViewById(R.id.recipe_image);
-            name = itemView.findViewById(R.id.recipe_name);
-            itemView.setOnClickListener(v ->{
-                listener.OnClick(recipeItemModelList.get(getBindingAdapterPosition()));
-            });
-        }
+    public View.OnClickListener getDefaultRequestBtnClickListener() {
+        return listener;
+    }
+
+    public void setDefaultRequestBtnClickListener(View.OnClickListener defaultRequestBtnClickListener) {
+        this.listener = defaultRequestBtnClickListener;
+    }
+
+    class ViewHolder  {
+        private  ImageView image;
+        private  TextView name;
+        private  TextView name2;
+        private  TextView detailsButton;
+
     }
 }
--- app/src/main/java/com/example/foodtinder/models/RecipeItemModel.java
+++ app/src/main/java/com/example/foodtinder/models/RecipeItemModel.java
@@ -1,7 +1,12 @@
 package com.example.foodtinder.models;
 
+import android.view.View;
+
 public class RecipeItemModel {
     private String name, image, url;
+
+    private View.OnClickListener requestBtnClickListener;
+
 
     public RecipeItemModel(String name, String image, String url) {
         this.name = name;
@@ -19,5 +24,13 @@ public class RecipeItemModel {
 
     public String getUrl() {
         return url;
+    }
+
+    public View.OnClickListener getRequestBtnClickListener() {
+        return requestBtnClickListener;
+    }
+
+    public void setRequestBtnClickListener(View.OnClickListener requestBtnClickListener) {
+        this.requestBtnClickListener = requestBtnClickListener;
     }
 }
