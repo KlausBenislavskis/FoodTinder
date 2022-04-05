afb8d748204df9f9f3e934aaac3202ab61c8056b
JakubPlatzek <jakub.platzek@gmail.com>
2022-04-05 12:43:11 +0200

    Implementation of foldable list with some spicy animations

--- app/src/main/java/com/example/foodtinder/adapters/FavouriteRecipeListAdapter.java
+++ app/src/main/java/com/example/foodtinder/adapters/FavouriteRecipeListAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodtinder.R;
 import com.example.foodtinder.models.RecipeItemModel;
